b5d0b10ed9dbf41fd17539c94d93959e98a1cdf8
Yuliia <juliy9050@gmail.com>
2025-06-01 15:11:36 +0200

    correction hw1

--- homework1/src/simpleLogger/logger.js
+++ homework1/src/simpleLogger/logger.js
@@ -1,9 +1,9 @@
 import fs from "node:fs/promises";
 
-const logMessage =async (message)=>{
-    const filepath = "./src/data/log.txt";
-    await fs.appendFile(filepath, `${message}`);
+const filepath = "./src/data/log.txt";
 
+const logMessage =async (message)=>{
+    await fs.appendFile(filepath, `\n${message}`);
 }
 
 export default logMessage;
