fa952612e1b45dea47092799fbaae7b3289d882f
Yuliia <juliy9050@gmail.com>
2025-06-16 14:35:05 +0200

    homework7

---
+++ homework7/src/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+
+import notFoundHandler from "./middelwares/notFoundHandler.js";
+
+const startServer = ()=>{
+    const app = express();
+
+    app.use(cors())
+    app.use(express.json())
+
+    app.use(notFoundHandler)
+
+    const port =process.env.PORT || 3000
+    app.listen(port, ()=>console.log(`Server running on ${port} port`))
+}
+export default startServer;
